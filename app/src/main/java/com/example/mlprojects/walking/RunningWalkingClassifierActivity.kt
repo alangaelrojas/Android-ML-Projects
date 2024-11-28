e48c70b6acb2a98f8513a335bbee9caa5bc34093
alangaelrojas <alan.garcia.contractor@realtor.com>
2024-11-27 23:35:30 -0600

    Theme updated and running and walking activity created

---
+++ app/src/main/java/com/example/mlprojects/walking/RunningWalkingClassifierActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mlprojects.walking
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mlprojects.R
+
+class RunningWalkingClassifierActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_running_walking_classifier)
+    }
+}
